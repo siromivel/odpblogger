c26d77a46cdaccc2ba50639697c73c6d0b1e9d38
Levi S. Morris <siromivel@gmail.com>
2014-07-15 12:09:27 -0400

    Image support added, tag deletion implemented but broken

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,16 @@
+class TagsController < ApplicationController
+	def show
+		@tag = Tag.find(params[:id])
+	end
+	def index
+		@tags = Tag.all
+	end
+	def destroy
+		@tag = Tag.find(params[:id])
+		@tag.delete
+		
+		flash.notice = "Tag '#{@tag.name}' Deleted!"
+		
+		redirect_to action: 'index'
+	end
+end
